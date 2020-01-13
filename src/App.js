583968977b6d1da774a78bb53d80bcfb27cdd06e
deeppaz <deeppaz@hotmail.com>
2020-01-13 17:58:42 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+//components
+import Navbar from './components/Navbar';
 
 //pages
 import home from './pages/home';
@@ -11,11 +13,14 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
+        <div className="container">
           <Switch>
             <Route exact path="/" component={home} />
             <Route exact path="/login" component={login} />
             <Route exact path="/signup" component={signup} />
           </Switch>
+        </div>
       </Router>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
-
+import Link from 'react-router-dom/Link';
 //mui işlevleri
 import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 
 
 class Navbar extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <AppBar position="fixed">
+                <Toolbar className="nav-container">
+                    <Button color="inherit" component={Link} to="/login">Login</Button>
+                    <Button color="inherit" component={Link} to="/">Home</Button>
+                    <Button color="inherit" component={Link} to="/signup">Signup</Button>
+                </Toolbar>
+            </AppBar>
         )
     }
 }
