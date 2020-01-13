0b513352e7d6c49c034af56d0592073b0d8b5f3a
deeppaz <deeppaz@hotmail.com>
2020-01-13 17:46:58 +0300

    install few things

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+//mui işlevleri
+import AppBar from '@material-ui/core/AppBar';
+
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Navbar
---
+++ src/pages/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class signup extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hey i'm signup page</h1>
+            </div>
+        )
+    }
+}
+
+export default signup
