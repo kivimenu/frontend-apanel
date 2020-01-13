2389975d2e15c445ce527513071a6cd57ba62b7b
deeppaz <deeppaz@hotmail.com>
2020-01-13 21:11:03 +0300

    update login and signup page

--- src/App.js
+++ src/App.js
@@ -2,25 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 //components
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 
 //pages
 import home from './pages/home';
-import login from './pages/login';
-import signup from './pages/signup';
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
-        <div className="container">
+        {/* <Navbar /> */}
           <Switch>
             <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
+            <Route exact path="/login" component={LogIn} />
+            <Route exact path="/signup" component={SignUp} />
           </Switch>
-        </div>
       </Router>
     </div>
   );
