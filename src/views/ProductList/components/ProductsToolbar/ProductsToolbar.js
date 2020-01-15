a18d3cb1b025cbe74726fbbb9bf546638dc86ea0
deeppaz <deeppaz@hotmail.com>
2020-01-16 02:50:49 +0300

    update add product

--- src/views/ProductList/components/ProductsToolbar/ProductsToolbar.js
+++ src/views/ProductList/components/ProductsToolbar/ProductsToolbar.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
+
+//material işevleri
 import { makeStyles } from '@material-ui/styles';
 import { Button } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Avatar from '@material-ui/core/Avatar';
+import InputAdornment from '@material-ui/core/InputAdornment';
+
+//material icons
+import EditIcon from '@material-ui/icons/Edit';
 
 import { SearchInput } from 'components';
 
@@ -17,6 +30,14 @@ const useStyles = makeStyles(theme => ({
   spacer: {
     flexGrow: 1
   },
+  HeRe: {
+    margin: '5%'
+  },
+  EditButton: {
+    marginTop: '-8%',
+    marginLeft: '3%',
+    position: 'relative'
+  },
   importButton: {
     marginRight: theme.spacing(1)
   },
@@ -25,10 +46,35 @@ const useStyles = makeStyles(theme => ({
   },
   searchInput: {
     marginRight: theme.spacing(1)
-  }
+  },
+  large: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
 }));
 
 const ProductsToolbar = props => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleImageChange = (event) => {
+    const image = event.target.files[0];
+    const formData = new FormData();
+    formData.append('image', image, image.name);
+    this.props.uploadImage(formData);
+  };
+
+  const handleEditPicture = () => {
+    const fileInput = document.getElementById('imageInput');
+    fileInput.click();
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   const { className, ...rest } = props;
 
   const classes = useStyles();
@@ -45,14 +91,64 @@ const ProductsToolbar = props => {
         <Button
           color="primary"
           variant="contained"
+          onClick={handleClickOpen}
         >
-          Add product
+          Ürün Ekle
         </Button>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+          <DialogTitle id="form-dialog-title">Ürün Ekleyin</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Lütfen ürün eklerken sorun çıkmaması için Tüm fieldları doldurun, aksi takdirde sorun çıkabilir!
+          </DialogContentText>
+            <hr className={classes.HeRe} />
+            <input type="file" id="imageInput" hidden="hidden" onChange={handleImageChange} />
+            <Avatar alt="Remy Sharp" src="/images/products/product_3.png" className={classes.large} />
+            <Button tip="Profil fotoğrafınızı Güncelleyin" className={classes.EditButton} onClick={handleEditPicture}>
+              <EditIcon />
+            </Button>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="urunadi"
+              label="Ürün Adı"
+              type="urunadi"
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="urunfiyat"
+              label="Ürün Fiyatı"
+              InputProps={{
+                startAdornment: <InputAdornment position="start">₺</InputAdornment>,
+              }}
+              type="number"
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="urunaciklama"
+              label="Ürün Açıklaması"
+              type="urunaciklama"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Iptal
+          </Button>
+            <Button onClick={handleClose} color="primary">
+              Ekle
+          </Button>
+          </DialogActions>
+        </Dialog>
       </div>
       <div className={classes.row}>
         <SearchInput
           className={classes.searchInput}
-          placeholder="Search product"
+          placeholder="Ürün Arayın"
         />
       </div>
     </div>
